337923b46931ef154875c16cf0f2195da3ad4ab5
Randhir2257 <randhirwork2257@gmail.com>
2026-07-24 10:59:59 +0530

    Add files via upload

---
+++ 1089.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a, b;
+
+cout << "Enter first number: ";
+cin >> a;
+
+cout << "Enter second number: ";
+cin >> b;
+
+if (a>b)
+cout << "Largest number:" << a;
+else
+cout << "Largest number;" << b;
+return 0;
+}
